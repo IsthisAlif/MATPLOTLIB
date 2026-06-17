d91891c05129938742e54b64c10bcc11f3113e82
IsthisAlif <abdulalif2003@gmail.com>
2026-06-17 16:43:00 +0800

    commit 2

---
+++ Labels.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([2023, 2024, 2025, 2026])
+y1 = np.array([15, 25, 30, 20])
+y2 = np.array([17, 23, 38, 5])
+y3 = np.array([13, 15, 20, 30])
+
+plt.title("Class size", fontsize=25,
+                        family="Arial",
+                        fontweight="bold",
+                        color="#2d4cfc")
+
+plt.xlabel("Year", fontsize=20,
+                   family="Arial",
+                   fontweight="bold",
+                   color="#2dbefc")
+
+plt.ylabel("Students", fontsize=20,
+                       family="Arial",
+                       fontweight="bold",
+                       color="#2dbefc")
+
+plt.tick_params(axis="both",
+                color="#2dbefc")
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.xticks(x)
+
+plt.show()
